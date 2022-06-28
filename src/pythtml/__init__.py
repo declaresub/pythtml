0829214c6646bab62a08e7dc8c6719e8c5591308
declaresub <charles@declaresub.com>
2022-06-28 11:50:45 -0400

    Add type hints; disable some useless warnings.

--- src/pythtml/__init__.py
+++ src/pythtml/__init__.py
@@ -6,11 +6,10 @@ It is not an HTML parser/validator; it assumes that user has some knowledge of H
 """
 
 try:
-    from importlib.metadata import metadata
+    from importlib.metadata import metadata  # type: ignore
 except ImportError:
-    from importlib_metadata import metadata
+    from importlib_metadata import metadata  # type: ignore
 
-from .elements import * #pylint: disable=wildcard-import
+from .elements import *
 
-
-__version__ = metadata(__name__)['version']
+__version__: str = metadata(__name__)["version"]
